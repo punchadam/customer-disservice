e7ac8b45e2995f36f620f591ba5f80b98f85fd2d
Adam Punch <punchadam712@gmail.com>
2026-03-28 18:30:09 -0700

    startup screen

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ using i8 = int8_t;
 using i16 = int16_t;
 using i32 = int32_t;
 
+const char* version = "Version 1.0.0";
+
 /*
 enum Hat {
   None,
@@ -18,6 +20,11 @@ enum Hat {
   Top,
   Baseball,
   Cowboy
+};
+enum Death {
+  None,
+  Fire,
+  Fall
 };
 struct Character {
   std::string name;
@@ -27,20 +34,26 @@ struct Character {
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-void setup() {
-  u8g2.begin();
-  u8g2.setFont(u8g2_font_ncenB14_tr);
+void startupScreen() {
+  u8g2.clearBuffer();
+  // u8g2.setFont(u8g2_font_jinxedwizards_tr);
+  u8g2.setFont(u8g2_font_oskool_tr);
+  u8g2.drawStr(0, 5 + 11, "Violet's");
+  // u8g2.setFont(u8g2_font_sticker100complete_tr);
+  u8g2.setFont(u8g2_font_disrespectfulteenager_tu);
+  u8g2.drawStr(3, 5 + 11 + 16, "CUSTOMER");
+  u8g2.drawStr(3, 5 + 11 + 16 + 2 + 16, "DISSERVICE");
+  //u8g2.setFont(u8g2_font_stylishcharm_tr);
+  //u8g2.drawStr(0, 64, version);
+  u8g2.sendBuffer();
+  delay(2000);
 }
 
-u8 xpos = 0, ypos = 20;
+void setup() {
+  u8g2.begin();
+  startupScreen();
+}
 
 void loop() {
-  u8g2.clearBuffer();
-  u8g2.drawStr(xpos, ypos, "Hello World");
-  u8g2.sendBuffer();
-
-  xpos = ++xpos % 128;
-  ypos = ++ypos % 64;
-
   delay(1000);
 }
